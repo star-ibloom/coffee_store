a07350b5b6201817a52e4a861f4bc56e8024b957
kerwin-wangdong <691466729@qq.com>
2018-07-12 09:29:44 +0800

    删除无用console.log

--- src/scripts/controllers/home.js
+++ src/scripts/controllers/home.js
@@ -1,35 +1,29 @@
 const homeModel = require('../models/home')
 
-const homeController={
-    renderNewest(){
-        // console.log(1)
+const homeController = {
+    renderNewest() {
         homeModel.getNewest()
-        .then(result=>{
-            var jsonNew = result.data.drinks;
-            var html ='';
-            for(let i =0;i<jsonNew.length;i++){
-                html +=`
+            .then(result => {
+                var jsonNew = result.data.drinks;
+                var html = '';
+                for (let i = 0; i < jsonNew.length; i++) {
+                    html += `
                 <li><img src="${jsonNew[i].imgPath}"></li>
                 `
-            }
-            // $(".hot-single-list").eq(0).html(html)
-        })
+                }
+            })
     },
-    renderHottest(){
+    renderHottest() {
         homeModel.getHottest()
-        .then(result=>{
-            var jsonHot = result.data;
-            console.log(jsonHot);
-            var html='';
-            for(let i=0;i<jsonHot.length;i++){
-                console.log(jsonHot[i].goodImg)
-                html+=`
+            .then(result => {
+                var jsonHot = result.data;
+                var html = '';
+                for (let i = 0; i < jsonHot.length; i++) {
+                    html += `
                 <li><img src="${jsonHot[i].goodImg}"></li>
                 `
-            } 
-            // $(".hot-single-list").eq(1).html(html)
-
-        })
+                }
+            })
     }
 }
 module.exports = homeController
